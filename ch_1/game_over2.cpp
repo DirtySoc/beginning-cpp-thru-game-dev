c499a76dfac331d0490b7b12490d7f0fbcc91639
DirtySoc <derek.willingham@outlook.com>
2020-06-03 19:19:55 +0000

    completed ch1

---
+++ ch_1/game_over2.cpp
@@ -0,0 +1,11 @@
+// This example shows how using a namespace allows you to shorten calls
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Game Over!!!" << endl;
+    return 0;
+}
+
